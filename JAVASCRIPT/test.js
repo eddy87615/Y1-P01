2af880c224bb4f328f9a0dd4f2dd1d7570d98b30
Eddy Chen <eddychen615@gmail.com>
2023-10-02 23:21:28 +0900

    test半角数字

--- JAVASCRIPT/test.js
+++ JAVASCRIPT/test.js
@@ -5,219 +5,221 @@
 const start = document.querySelector('.start');
 const test = document.querySelector('.test');
 
-start.addEventListener(
-    'click',function(){
-        test.classList.add("test_show");
-    }
-);
+start.addEventListener('click', function () {
+  test.classList.add('test_show');
+});
 
 //-----Quiz Whole-----//
 
 const quizArr = [
-    // {
-    //     id: 0,
-    //     title: "",
-    //     content: ``,
-    //     img: ``,
-    //     bubble:``
-    // },
-    {
-        id: 0,
-        title: "<br>１．私はストレスを解消する方法がある。",
-        content: `研究による、ストレスコントロール力が高ければ、問題に直面しても乗り越えることができて、前向きに仕事に取り組むことができる。`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 1,
-        title: "<br>２．私は厳しい環境で成長できる。",
-        content: `私を破壊するに至らないすべてのことが、私をさらに強くする。<br>——ニーチェ——`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 2,
-        title: "<br><br>３．私は努力が報われると信じている。",
-        content: ``,
-        img: `img/努力_アートボード 1.svg`,
-        bubble:``
-    },
-    {
-        id: 3,
-        title: "<br>４．私はスケジュール管理ができる。",
-        content: `課題が多いから、自分のスケジュールをちゃんと管理しないとやばいだよ！`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 4,
-        title: "<br><br>５．私は臨機応変に物事を対応できる。",
-        content: ``,
-        img: `img/臨機応変.svg`,
-        bubble:``
-    },
-    {
-        id: 5,
-        title: "<br>６．私は自習することができる。",
-        content: `自主的に勉強するのが大事だよ！ `,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 6,
-        title: "<br>７．私は知らないことに興味がある。",
-        content: `IT技術の更新が早くていつも新しい知識を得られる。`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 7,
-        title: "<br>８．私は複雑な問題を解決したら達成感を感じる。",
-        content: `コーディングがさすが難しいよね...`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 8,
-        title: "<br>９．私は自分で調べて問題を解決することができる。",
-        content: `知らないことは他人に聞く以外自分で調べるのも大事だよ！`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 9,
-        title: "<br>１０．私は知らないことを知らないままをするのが嫌だ。",
-        content: `知らないまま眠れないよね？`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 10,
-        title: "<br>１１．私は一人作業よりチームで作業するのが好き。",
-        content: `Webデザイン科はチームワークを重視している学科だよ！`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 11,
-        title: "<br><br>１２．私はクラスメイトと課題を話し合うことができる。",
-        content: ``,
-        img: `img/meeting01.svg`,
-        bubble:``
-    },
-    {
-        id: 12,
-        title: "<br>１３．私は質問があれば先輩/先生に聞く。",
-        content: `先生も先輩もみんなの力になれる！`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 13,
-        title: "<br><br>１４．私は黙りっぱなしより自分から話す方が好き。",
-        content: ``,
-        img: `img/talk.svg`,
-        bubble:``
-    },
-    {
-        id: 14,
-        title: "<br><br>１５．私は自分の役割をちゃんとできて、他人の迷惑をかけない。",
-        content: ``,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 15,
-        title: "<br>１６．私は産学連携に興味がある。",
-        content: `専門学校にはやっぱり就職だろう？これからどんな事があるのを先に知った方がいいと思うね`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 16,
-        title: "<br>１７．私はパソコンで働くことを楽しみしている。",
-        content: `エンジニアもデザイナーも！パソコンがあれば仕事ができる！！`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 17,
-        title: "<br>１８．私はプロジェクトを参加して経験を貯めるのに興味がある。",
-        content: `エンジニアもデザイナーも！パソコンがあれば仕事ができる！！`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 18,
-        title: "<br>１９．仕事で新しい知識を学ぶのが楽しい。",
-        content: `ITの知識毎日更新しているので、いつも新しい技術を学ぶ心構えが必要！`,
-        img: ``,
-        bubble:``
-    },
-    {
-        id: 19,
-        title: "<br><br>２０．簡単な手段で複雑な問題を解決することができる。",
-        content: ``,
-        img: ``,
-        bubble:``
-    }
-]
+  // {
+  //     id: 0,
+  //     title: "",
+  //     content: ``,
+  //     img: ``,
+  //     bubble:``
+  // },
+  {
+    id: 0,
+    title: '<br>1.私はストレスを解消する方法がある。',
+    content: `研究による、ストレスコントロール力が高ければ、問題に直面しても乗り越えることができて、前向きに仕事に取り組むことができる。`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 1,
+    title: '<br>2.私は厳しい環境で成長できる。',
+    content: `私を破壊するに至らないすべてのことが、私をさらに強くする。<br>——ニーチェ——`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 2,
+    title: '<br><br>3.私は努力が報われると信じている。',
+    content: ``,
+    img: `img/努力_アートボード 1.svg`,
+    bubble: ``,
+  },
+  {
+    id: 3,
+    title: '<br>4.私はスケジュール管理ができる。',
+    content: `課題が多いから、自分のスケジュールをちゃんと管理しないとやばいだよ！`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 4,
+    title: '<br><br>5.私は臨機応変に物事を対応できる。',
+    content: ``,
+    img: `img/臨機応変.svg`,
+    bubble: ``,
+  },
+  {
+    id: 5,
+    title: '<br>6.私は自習することができる。',
+    content: `自主的に勉強するのが大事だよ！ `,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 6,
+    title: '<br>7.私は知らないことに興味がある。',
+    content: `IT技術の更新が早くていつも新しい知識を得られる。`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 7,
+    title: '<br>8.私は複雑な問題を解決したら達成感を感じる。',
+    content: `コーディングがさすが難しいよね...`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 8,
+    title: '<br>9.私は自分で調べて問題を解決することができる。',
+    content: `知らないことは他人に聞く以外自分で調べるのも大事だよ！`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 9,
+    title: '<br>10.私は知らないことを知らないままをするのが嫌だ。',
+    content: `知らないまま眠れないよね？`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 10,
+    title: '<br>11.私は一人作業よりチームで作業するのが好き。',
+    content: `Webデザイン科はチームワークを重視している学科だよ！`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 11,
+    title: '<br><br>12.私はクラスメイトと課題を話し合うことができる。',
+    content: ``,
+    img: `img/meeting01.svg`,
+    bubble: ``,
+  },
+  {
+    id: 12,
+    title: '<br>13.私は質問があれば先輩/先生に聞く。',
+    content: `先生も先輩もみんなの力になれる！`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 13,
+    title: '<br><br>14.私は黙りっぱなしより自分から話す方が好き。',
+    content: ``,
+    img: `img/talk.svg`,
+    bubble: ``,
+  },
+  {
+    id: 14,
+    title: '<br><br>15.私は自分の役割をちゃんとできて、他人の迷惑をかけない。',
+    content: ``,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 15,
+    title: '<br>16.私は産学連携に興味がある。',
+    content: `専門学校にはやっぱり就職だろう？これからどんな事があるのを先に知った方がいいと思うね`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 16,
+    title: '<br>17.私はパソコンで働くことを楽しみしている。',
+    content: `エンジニアもデザイナーも！パソコンがあれば仕事ができる！！`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 17,
+    title: '<br>18.私はプロジェクトを参加して経験を貯めるのに興味がある。',
+    content: `エンジニアもデザイナーも！パソコンがあれば仕事ができる！！`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 18,
+    title: '<br>19.仕事で新しい知識を学ぶのが楽しい。',
+    content: `ITの知識毎日更新しているので、いつも新しい技術を学ぶ心構えが必要！`,
+    img: ``,
+    bubble: ``,
+  },
+  {
+    id: 19,
+    title: '<br><br>20.簡単な手段で複雑な問題を解決することができる。',
+    content: ``,
+    img: ``,
+    bubble: ``,
+  },
+];
 
 const rank = [
-    {
-        id: 0,
-        rank:"img/A.svg",
-        commend:"<b>おめでとうございます！</b><br>優秀なWebデザイン科の学生になる準備はもう完璧にできたそうだよ～",
-    },
-    {
-        id: 1,
-        rank:"img/B.svg",
-        commend:"<b>おめでとうございます！</b><br>Webデザイン科の学生になる準備はもう大体できたそうだよ～",
-    },
-    {
-        id: 2,
-        rank:"img/C.svg",
-        commend:"<b>お疲れ様です</b><br>Webデザイン科の学生になるのはあなたに対して大変そうだがなんでも乗り越えるだろうか。",
-    },
-    {
-        id: 3,
-        rank:"img/D.svg",
-        commend:"<b>頑張ろう</b><br>あ～Webデザイン科の学生になる準備はまだだろう。もっと調べて、深く知っていこうか。",
-    },
-    {
-        id: 4,
-        rank:"img/E.svg",
-        commend:"<b>頑張ろう！！</b><br>Webデザイン科に入ったら厳しい挑戦に違いないだろう。もう一回診断をやってみようか。",
-    }
-]
+  {
+    id: 0,
+    rank: 'img/A.svg',
+    commend:
+      '<b>おめでとうございます！</b><br>優秀なWebデザイン科の学生になる準備はもう完璧にできたそうだよ～',
+  },
+  {
+    id: 1,
+    rank: 'img/B.svg',
+    commend:
+      '<b>おめでとうございます！</b><br>Webデザイン科の学生になる準備はもう大体できたそうだよ～',
+  },
+  {
+    id: 2,
+    rank: 'img/C.svg',
+    commend:
+      '<b>お疲れ様です</b><br>Webデザイン科の学生になるのはあなたに対して大変そうだがなんでも乗り越えるだろうか。',
+  },
+  {
+    id: 3,
+    rank: 'img/D.svg',
+    commend:
+      '<b>頑張ろう</b><br>あ～Webデザイン科の学生になる準備はまだだろう。もっと調べて、深く知っていこうか。',
+  },
+  {
+    id: 4,
+    rank: 'img/E.svg',
+    commend:
+      '<b>頑張ろう！！</b><br>Webデザイン科に入ったら厳しい挑戦に違いないだろう。もう一回診断をやってみようか。',
+  },
+];
 //-----defination-----//
 
-const quiz_title = document.querySelector(".quiz_title");
-const quiz_content = document.querySelector(".quiz_content");
-const btns = document.querySelectorAll("button");
+const quiz_title = document.querySelector('.quiz_title');
+const quiz_content = document.querySelector('.quiz_content');
+const btns = document.querySelectorAll('button');
 
-const bgcBox = document.querySelector("bgc_pic");
-const bgc_img = document.createElement("img");
+const bgcBox = document.querySelector('bgc_pic');
+const bgc_img = document.createElement('img');
 
-const rank_show = document.querySelector(".score_page");
+const rank_show = document.querySelector('.score_page');
 
-const ranking = document.querySelector(".rank");
-const commend = document.querySelector(".commend");
+const ranking = document.querySelector('.rank');
+const commend = document.querySelector('.commend');
 
-const jec_btn = document.querySelector(".jec");
-const get_doc = document.querySelector(".get_doc");
-const hotpage = document.querySelector(".hotpage");
+const jec_btn = document.querySelector('.jec');
+const get_doc = document.querySelector('.get_doc');
+const hotpage = document.querySelector('.hotpage');
 
-const rankingPic = document.createElement("img");
+const rankingPic = document.createElement('img');
 
-const counterElement = document.querySelector(".counter");
+const counterElement = document.querySelector('.counter');
 
 console.log(counterElement);
 
 console.log(jec_btn);
 console.log(get_doc);
 console.log(hotpage);
-
 
 let counter = 1;
 let score = -3;
@@ -227,28 +229,28 @@ let score = -3;
 let count = 0;
 const totalcount = 20;
 
-function updatecount(){
-    counterElement.textContent =  `${count}/${totalcount}`;
+function updatecount() {
+  counterElement.textContent = `${count}/${totalcount}`;
 }
 
 updatecount();
 
-function pluscount(){
-    if(count < totalcount){
-        count++;
-        updatecount();
-    }
+function pluscount() {
+  if (count < totalcount) {
+    count++;
+    updatecount();
+  }
 }
 
 btns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        if(counter > quizArr.length){
-            //...
-        }else{
-            pluscount();
-        }
-    })
-})
+  btn.addEventListener('click', () => {
+    if (counter > quizArr.length) {
+      //...
+    } else {
+      pluscount();
+    }
+  });
+});
 
 // counterElement.addEventListener('click', pluscount);
 
@@ -256,122 +258,108 @@ btns.forEach((btn) => {
 
 //--------------------//
 
-
 function changeQuiz() {
-    quiz_title.innerHTML = quizArr[counter - 1].title;
+  quiz_title.innerHTML = quizArr[counter - 1].title;
 
-    quiz_content.innerHTML = quizArr[counter - 1].content;
+  quiz_content.innerHTML = quizArr[counter - 1].content;
 
+  //-----内容有無判断-----//
 
-//-----内容有無判断-----//
+  if (quiz_content.textContent === '') {
+    quiz_content.classList.add('quiz_content_hide');
+  } else {
+    quiz_content.classList.remove('quiz_content_hide');
+  }
 
-    if(quiz_content.textContent === ''){
+  //--------------------//
 
-        quiz_content.classList.add("quiz_content_hide")
-    }else{
-        quiz_content.classList.remove("quiz_content_hide")
+  //-----画像インプット-----//
 
-    };
+  bgc_img.src = quizArr[counter - 1].img;
+  document.querySelector('.bgc_pic').appendChild(bgc_img);
 
-//--------------------//
-
-//-----画像インプット-----//
-
-    bgc_img.src = quizArr[counter - 1].img;
-    document.querySelector('.bgc_pic').appendChild(bgc_img);
-
-//--------------------//
-    
+  //--------------------//
 }
-changeQuiz()
+changeQuiz();
 
 btns.forEach((btn) => {
-    btn.addEventListener("click", () => {
+  btn.addEventListener('click', () => {
+    if (counter > quizArr.length) {
+      if (btn.className.match('yes')) {
+        score += 5;
+      } else if (btn.className.match('no')) {
+        score += 1;
+      } else {
+        score += 3;
+      }
 
-        if(counter > quizArr.length) {
-            if(btn.className.match("yes")){
-                score+=5;
-            }else if(btn.className.match("no")){
-                score+=1;
-            }else{score+=3};
+      console.log(counter);
 
-            console.log(counter);
+      if (counter >= 21) {
+        rank_show.classList.add('score_show');
 
-            if(counter >= 21){
-                rank_show.classList.add("score_show")
+        console.log(score);
 
-                console.log(score);
+        //-----ranking-----//
 
-                //-----ranking-----//
+        if (score >= 90) {
+          rankingPic.src = rank[0].rank;
+          document.querySelector('.rank').appendChild(rankingPic);
 
-                if( score >= 90 ){
+          commend.innerHTML = rank[0].commend;
 
-                    rankingPic.src = rank[0].rank;
-                    document.querySelector('.rank').appendChild(rankingPic);
+          jec_btn.classList.add('jec_hide');
+          hotpage.classList.add('hotpage_hide');
+        } else if (score >= 80) {
+          rankingPic.src = rank[1].rank;
+          document.querySelector('.rank').appendChild(rankingPic);
 
-                    commend.innerHTML = rank[0].commend;
+          commend.innerHTML = rank[1].commend;
 
-                    jec_btn.classList.add("jec_hide");
-                    hotpage.classList.add("hotpage_hide");
+          jec_btn.classList.add('jec_hide');
+          hotpage.classList.add('hotpage_hide');
+        } else if (score >= 70) {
+          rankingPic.src = rank[2].rank;
+          document.querySelector('.rank').appendChild(rankingPic);
 
-                }else if( score >= 80 ){
+          commend.innerHTML = rank[2].commend;
 
-                    rankingPic.src = rank[1].rank;
-                    document.querySelector('.rank').appendChild(rankingPic);
+          jec_btn.classList.add('jec_hide');
+          hotpage.classList.add('hotpage_hide');
+        } else if (score >= 60) {
+          rankingPic.src = rank[3].rank;
+          document.querySelector('.rank').appendChild(rankingPic);
 
-                    commend.innerHTML = rank[1].commend;
+          commend.innerHTML = rank[3].commend;
 
-                    jec_btn.classList.add("jec_hide");
-                    hotpage.classList.add("hotpage_hide");
-
-                }else if( score >= 70 ){
-
-                    rankingPic.src = rank[2].rank;
-                    document.querySelector('.rank').appendChild(rankingPic);
-
-                    commend.innerHTML = rank[2].commend;
-
-                    jec_btn.classList.add("jec_hide");
-                    hotpage.classList.add("hotpage_hide");
-
-                }else if( score >= 60 ){
-
-                    rankingPic.src = rank[3].rank;
-                    document.querySelector('.rank').appendChild(rankingPic);
-
-                    commend.innerHTML = rank[3].commend;
-
-                    get_doc.classList.add("get_doc_hide");
-                    
-
-                }else{
-
-                    rankingPic.src = rank[4].rank;
-                    document.querySelector('.rank').appendChild(rankingPic);
-
-                    commend.innerHTML = rank[4].commend;
-
-                    get_doc.classList.add("get_doc_hide");
-
-                };
-
-            }else{
-                rank_show.classList.remove("score_show")
-            }
-
-            // quiz_title.innerHTML = "点数：" + score
-            // quiz_content.innerHTML = ""
+          get_doc.classList.add('get_doc_hide');
         } else {
-            if(btn.className.match("yes")){
-                score+=5;
-            }else if(btn.className.match("no")){
-                score+=1;
-            }else{score+=3};
-            changeQuiz()
+          rankingPic.src = rank[4].rank;
+          document.querySelector('.rank').appendChild(rankingPic);
 
-            console.log(counter);
-            console.log(score);
+          commend.innerHTML = rank[4].commend;
+
+          get_doc.classList.add('get_doc_hide');
         }
-            counter++
-    })
+      } else {
+        rank_show.classList.remove('score_show');
+      }
+
+      // quiz_title.innerHTML = "点数：" + score
+      // quiz_content.innerHTML = ""
+    } else {
+      if (btn.className.match('yes')) {
+        score += 5;
+      } else if (btn.className.match('no')) {
+        score += 1;
+      } else {
+        score += 3;
+      }
+      changeQuiz();
+
+      console.log(counter);
+      console.log(score);
+    }
+    counter++;
+  });
 });
